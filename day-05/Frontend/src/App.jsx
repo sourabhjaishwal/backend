c3641188dad8ec95a8f4f034001e1b994a831d65
sourabhjaishwal <jaishwalsourabh2000@gmail.com>
2026-07-27 23:23:25 +0530

    feat: initialize frontend with React, Vite, and basic note-taking functionality
    - Add package.json with dependencies and scripts for development
    - Create favicon and icons SVG files for branding
    - Implement main App component to handle notes fetching, creation, and deletion
    - Add CSS styles for layout and design of the notes app
    - Set up entry point with main.jsx and integrate with Vite configuration

---
+++ day-05/Frontend/src/App.jsx
@@ -0,0 +1,73 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function fetchNotes() {
+    axios.get("http://localhost:3000/api/notes").then((res) => {
+      setNotes(res.data.notes);
+    });
+  }
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const { title, description } = e.target.elements;
+    console.log(title.value, description.value);
+
+    axios
+      .post("http://localhost:3000/api/notes", {
+        title: title.value,
+        description: description.value,
+      })
+      .then((res) => {
+        console.log(res.data);
+        fetchNotes();
+      });
+  }
+
+  function handleDeleteNote(noteId) {
+    axios.delete("http://localhost:3000/api/notes/" + noteId).then((res) => {
+      console.log(res.data);
+      fetchNotes();
+    });
+  }
+
+  return (
+    <>
+      <h1 className="header">Simple Notes App</h1>
+      <form className="note-create-form" onSubmit={handleSubmit}>
+        <input name="title" type="text" placeholder="Enter your title" />
+        <input
+          name="description"
+          type="text"
+          placeholder="Enter your description"
+        />
+        <button>Add Note</button>
+      </form>
+      <div className="notes">
+        {notes.map((note) => {
+          return (
+            <div className="note">
+              <h1>{note.title}</h1>
+              <p>{note.description}</p>
+              <button
+                onClick={() => {
+                  handleDeleteNote(note._id);
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default App;
